d2819275bf629bd6ac42f027b17af02107830943
Euler d'Moogle <cphonx@users.noreply.github.com>
2018-10-20 16:59:11 -0700

    just a test

---
+++ common/ctest.cpp
@@ -0,0 +1,10 @@
+#include "ctest.h"
+#include <iostream>
+
+namespace test1
+{
+    void printhello()
+    {
+       std::cout << "From inside a library...";
+    }
+}
---
+++ common/ctest.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+namespace test1
+{
+    void printhello();
+}
